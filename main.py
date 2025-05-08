8e892c42115dd28663cbd832ac6a139440b9be66
Sergi Diaz Lopez <sergidiazlopez@gmail.com>
2025-05-08 13:25:22 +0200

    prueba yolo para raspberry

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-from time import sleep
-while True:
-	print("Hello World")
-	sleep(2)
+# from picamera import PiCamera
+from ultralytics import YOLO
+import cv2
+
+# Initialize the camera
+# camera = PiCamera()
+
+# Capture an image
+# camera.capture('image.jpg')
+
+# Close the camera
+# camera.close()
+
+# Load a pretrained model
+model = YOLO('yolov8n.pt')
+
+# Load the captured image
+image = cv2.imread('image.png')
+
+# Perform object detection
+results = model(image)
+
+# Display the results
+#for result in results:
+#    result.show()
