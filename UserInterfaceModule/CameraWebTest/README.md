b077e34cd2826922e02527e5ca2166b7582d7165
Sergi <Sergi@DESKTOP-1OTSKS9>
2025-05-15 20:54:53 +0200

    programa para acceder a las imagenes de la camara de la raspberry desde el navegador (no testeado!!)

---
+++ UserInterfaceModule/CameraWebTest/README.md
@@ -0,0 +1,4 @@
+# Camera Web Test
+
+Programa de prueba que lanza una página web con la imágen de la cámara de la raspberry en tiempo real para poder guardarlas desde el pc.
+
---
+++ UserInterfaceModule/CameraWebTest/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, Response
+# from picamera2 import Picamera2
+import cv2
+import time
+
+app = Flask(__name__)
+# picam2 = Picamera2()
+# picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+# picam2.start()
+
+# def generate_frames():
+#     while True:
+#         frame = picam2.capture_array()
+#         ret, buffer = cv2.imencode('.jpg', frame)
+#         if not ret:
+#             continue
+#         frame_bytes = buffer.tobytes()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+def generate_frames():
+    # frame = picam2.capture_array()
+    frame = cv2.imread('D://Desktop//prueba.jpeg')
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame_bytes = buffer.tobytes()
+    yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
